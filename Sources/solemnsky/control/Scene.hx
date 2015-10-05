40cdecb88a39b27be211c3dcbe48f9511ecbc74f
Chris Gadzinski <zenmags@gmail.com>
2015-10-05 18:36:34 +0000

    thinking about rolling a scene graph, but primarily interested in this weird graphics issue

--- Sources/solemnsky/control/Scene.hx
+++ Sources/solemnsky/control/Scene.hx
@@ -1,5 +1,25 @@
 package solemnsky.control;
 
+/**
+ * Represents a set of 
+ */
+class DrawMethods {
+    private var transform:Dynamic;
+
+    public function new(transform:Dynamic) {
+        this.transform = transform;
+    }
+
+    public function drawSomething() {
+        // draw something with this.transform in scope
+    }
+}
+
+/**
+ * Represents a scene object, that can be drawn.
+ */
 class Scene {
-    
+    public function new() {
+
+    } 
 }
