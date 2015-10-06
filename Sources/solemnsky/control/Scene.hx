291e97694499b3170ee75f128337a93a8b52bbf6
Chris Gadzinski <zenmags@gmail.com>
2015-10-06 21:00:49 +0000

    using builtin transformation stack

--- Sources/solemnsky/control/Scene.hx
+++ Sources/solemnsky/control/Scene.hx
@@ -15,8 +15,7 @@ class Scene {
     public var prims:Array<DrawPrim> = [];
     public var children:Array<Scene> = [];
     public var trans:Mat3 = Mat3.identity();
-    // public var alpha:Float = 1;
-
+    public var alpha:Float = 1;
 
     /*************************************************************************/
     /* constructor
@@ -24,37 +23,5 @@ class Scene {
 
     public function new() {
         // nothing
-    }
-
-    /*************************************************************************/
-    /* rendering helper (drawWith)
-    /*************************************************************************/
-
-    private static function transformPrim(mat:Mat3, prim:DrawPrim):DrawPrim {
-        switch (prim) {
-            case DrawCircle(pos, radius): 
-                return DrawCircle(mat.apply(pos), radius);
-        }
-    }
-
-    public function flattenPrims(parentMat:Mat3):Array<DrawPrim> {
-        var nextMat:Mat3 = trans.compose(parentMat); 
-            // the transform matrix sent to children
-
-        var allPrims:Array<DrawPrim> = [];
-        for (prim in prims) {
-            allPrims.push(transformPrim(parentMat, prim));
-        }
-        for (child in children) {
-            allPrims = allPrims.concat(child.flattenPrims(nextMat));
-        }
-        return allPrims;
-    }
-
-    public function drawWith(render:DrawPrim->Void):Void {
-        var flattened = flattenPrims(Mat3.identity());
-        for (prim in flattened) {
-            render(prim);
-        }
     }
 }
