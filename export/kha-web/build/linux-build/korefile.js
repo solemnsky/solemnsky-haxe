db48b35f84e11b692893da1139bc4be6a14a8518
Chris Gadzinski <zenmags@gmail.com>
2015-10-16 14:05:56 +0000

    huh so .gitignore doesn't traverse or something

--- export/kha-web/build/linux-build/korefile.js
+++
@@ -1,5 +0,0 @@
-var project = new Project('HaxeCrossCode');
-project.addFiles('Sources/**.h', 'Sources/**.cpp', 'Sources/**.metal');
-project.addExcludes('Sources/src/__main__.cpp');
-project.addIncludeDirs('Sources/include');
-return project;
