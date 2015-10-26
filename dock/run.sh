4caf81664e7864636f833cbf512fbd392117b676
Chris Gadzinski <zenmags@gmail.com>
2015-10-26 14:50:07 +0000

    dockerfile work

--- dock/run.sh
+++ dock/run.sh
@@ -1,2 +1,2 @@
 # runs our dockerized kha with the project mounted to /tmp/haxe
-docker run -it --name kha --rm -v "$(pwd)":/tmp/haxe -w /tmp/haxe solemnsky/kha /bin/bash 
+docker run -it --rm -v "$(pwd)":/tmp/haxe -w /tmp/haxe solemnsky/kha /bin/bash 
